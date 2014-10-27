7d9e1d5b4412fcebaf094b0e8d009044402f2f32
dowon <shovelswingman@gmail.com>
2014-10-27 23:18:13 +0900

    Add crcCheck function

--- src/com/dowon/tcpclient/SendPacket.java
+++ src/com/dowon/tcpclient/SendPacket.java
@@ -1,5 +1,31 @@
 package com.dowon.tcpclient;
 
-public class SendPacket {
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
 
+public class SendPacket {
+	private CRC32 crc32;
+	private byte[] packet;
+	
+	
+
+	//ADD TO PACKET CRC
+	public void addCRC32() {
+		crc32 = new CRC32();
+		crc32.update(packet);
+		Long crcvalue = crc32.getValue();
+		
+		byte[] temp = packet;
+		packet = new byte[packet.length + Long.SIZE];
+		System.arraycopy(temp, 0, packet, 0, temp.length);
+		
+		byte[] crc_array = longToBytes(crcvalue);
+		System.arraycopy(crc_array, 0, packet, packet.length - 5, crc_array.length);
+	}
+	
+	public byte[] longToBytes(long value) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.SIZE);
+		buffer.putLong(value);
+		return buffer.array();
+	}
 }
